2fa7b4f108b575f5be13ffc2c62b97e268d506ff
MUHAMMAD FAIQ MUBAROK <faiq@MacBook-Air-MUHAMMAD.local>
2024-11-30 07:32:39 +0700

    new set up backend

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error(err));
 
